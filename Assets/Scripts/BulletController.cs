8cc53a09e7bdb5bf089ca3231a9297767630036f
GoutteDooo <goudal.Donovan@live.fr>
2025-08-05 12:01:28 +0200

    chore(Input): Ajout de l'input 'Fire' pour click gauche

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    public float speed = 0.5f;
+    public float speed = 5f;
     Vector2 lastVelocity;
 
     private Rigidbody2D rb;
@@ -13,7 +13,7 @@ public class BulletController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>(); // Access player's Rigidbody.
 
         // On applique une force initiale vers le haut et sur le côté
-        rb.AddForce(new Vector2(2f, 0f), ForceMode2D.Impulse);
+        rb.AddForce(new Vector2(5f, 2f), ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
@@ -26,7 +26,7 @@ public class BulletController : MonoBehaviour
     {
         Debug.Log("La balle a touché : " + collision.gameObject.name);
         var speed = lastVelocity.magnitude;
-        var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+        var direction = Vector2.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
 
         rb.linearVelocity = direction * Mathf.Max(speed, 0f);
 
