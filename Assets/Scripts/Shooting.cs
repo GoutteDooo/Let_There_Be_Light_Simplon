943201ac19b8eb6a877e9e1fb2ace486b5024b99
GoutteDooo <goudal.Donovan@live.fr>
2025-08-05 13:47:14 +0200

    Script of Shooting set, and variables ok for PlayerTest lvl

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,11 +4,15 @@ public class Shooting : MonoBehaviour
 {
     private Camera _mainCam;
     private Vector3 mousePos;
+    public GameObject bullet; // bullet that will be instantiated
+    public Transform bulletTransform; // gun
+    public bool canFire; // to know where player can fire
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        canFire = true;
     }
 
     // Update is called once per frame
@@ -21,5 +25,13 @@ public class Shooting : MonoBehaviour
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        if (!canFire) return; // TODO : Set canFire to true when there is no bullet left in the room
+
+        if (Input.GetMouseButton(0) && canFire)
+        {
+            canFire = false;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
     }
 }
