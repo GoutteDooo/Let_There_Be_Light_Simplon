a0b90e19a0ea4b612cf166cfcb3dc0d52a7265f3
GoutteDooo <goudal.Donovan@live.fr>
2025-08-07 02:33:55 +0200

    feat: lvl 5 designé

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -14,7 +14,7 @@ public class RoomManager : MonoBehaviour
 
     void Start()
     {
-        currentRoomIndex = 0;//DEV
+        currentRoomIndex = 4;//DEV
         LoadRoom(currentRoomIndex);//DEV
         //LoadRoom(0); // Début du jeu
     }
