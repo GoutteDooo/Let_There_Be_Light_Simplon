52cff12e0674b12ce669f5ff327180438395586f
HazelCunuder <basile.cunuder@gmail.com>
2025-08-07 09:10:59 +0200

    refactor: move scripts into correct folder

---
+++ Assets/Scripts/BulletsCountdownLogic.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletsCountdownLogic : MonoBehaviour
+{
+    public Text bulletsInGun;
+    private Shooting shooting;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        DisplayBullets();
+    }
+
+    public void DisplayBullets()
+    {
+        if (shooting != null)
+        {
+            bulletsInGun.text = shooting.GetBulletLefts().ToString();
+        }
+    }
+    public void RefreshShootingReference()
+    {
+        shooting = GameObject.FindFirstObjectByType<Shooting>();
+    }
+}
+
