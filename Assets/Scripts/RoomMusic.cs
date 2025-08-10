568f730a90d285e6438ecdf6f65f5f2043f27ec7
GoutteDooo <goudal.Donovan@live.fr>
2025-08-10 09:18:42 +0200

    Ajout composant RoomMusic à attacher à chaque roomPrefab

---
+++ Assets/Scripts/RoomMusic.cs
@@ -0,0 +1,12 @@
+// RoomMusic.cs
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class RoomMusic : MonoBehaviour
+{
+    public AudioClip clip;
+    public bool loop = true;
+    public float fade = 1.5f;
+    [Tooltip("Lance la musique dès l'apparition de la room")]
+    public bool playOnSpawn = true;
+}
