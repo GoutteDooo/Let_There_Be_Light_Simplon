b87c4bf3cce176c03c378cee4b4109a1b1f95d87
HazelCunuder <basile.cunuder@gmail.com>
2025-08-11 11:00:53 +0200

    feat: add other pic for end screen

--- Assets/Scripts/EasterEgg.cs
+++ Assets/Scripts/EasterEgg.cs
@@ -12,3 +12,17 @@ public class EasterEgg : MonoBehaviour
         }
     }
 }
+
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Easter Egg Activated");
+        if (collision.gameObject.GetComponent<BulletController>() != null)
+        {
+            RoomManager.Instance.LoadRoom(14); // Singleton call
+        }
+    }
+}
